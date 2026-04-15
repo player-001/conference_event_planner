fec5fd5ff3dbcd4116fe488a3b4c499aff3954df
player-001 <mohitworksop@gmail.com>
2026-04-15 12:40:16 -0400

    updated

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -7,6 +7,7 @@ const ConferenceEvent = () => {
     const [showItems, setShowItems] = useState(false);
     const [numberOfPeople, setNumberOfPeople] = useState(1);
     const venueItems = useSelector((state) => state.venue);
+    const avItems = useSelector((state) => state.av)
     const dispatch = useDispatch();
     const remainingAuditoriumQuantity = 3 - venueItems.find(item => item.name === "Auditorium Hall (Capacity:200)").quantity;
 
@@ -157,7 +158,9 @@ const ConferenceEvent = () => {
 
                                 </div>
                                 <div className="addons_selection">
-
+                                    
+                                    
+                                    
                                 </div>
                                 <div className="total_cost">Total Cost:</div>
 
